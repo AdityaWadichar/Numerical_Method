8734608c38d7c9c443dac446e18e7f2bede5eca6
Aditya Wadichar <adityawadichar85@gmail.com>
2021-06-04 01:39:50 +0530

    Update phiKepler.py

--- Basic implementations/phiKepler.py
+++ Basic implementations/phiKepler.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def phiKepler(q0, p0, t, mu):
-    r0 = (q0[0][0]**2 + q0[1][0]**2)**(1/2)
+    h=0.1
+    r0 = ((q0[0][0])**2 + (q0[1][0])**2)**(1/2)
     u = q0[0][0]*p0[0][0] + q0[1][0]*p0[1][0]
     E = (1/2)*(p0[0][0]*p0[0][0] + p0[1][0]*p0[1][0]) - mu/r0
     a = -mu/(2*E)
@@ -12,15 +13,15 @@ def phiKepler(q0, p0, t, mu):
     p = np.zeros([2, len(t)])
     x = np.zeros(len(t))
     x[0] = w*t[0]*a/r0
-    print(q[1][:])
+    #print(q[1][:])
     q[0][0] = q0[0]
     q[0][1] = q0[1]
     p[0][0] = p0[0]
     p[0][1] = p0[1]
     for i in range(len(t)-1):
-        num = x[i] - sigma*np.sin(x[i]) + si*(1-np.cos(x[i])) - w*t[1]
+        num = x[i] - sigma*np.sin(x[i]) + si*(1-np.cos(x[i])) - w*t[i]
         den = 1 - sigma*np.cos(x[i]) + si*np.sin(x[i])
-        x[i+1] = x[1] - (num/den)
+        x[i+1] = x[i] - (num/den)
 
     for i in range(len(t)):
         fq = 1 + (np.cos(x[i])-1)*a/r0
@@ -33,4 +34,8 @@ def phiKepler(q0, p0, t, mu):
         p[0][i] = fp*q0[0][0] + gp*p0[0][0]
         p[1][i] = fp * q0[1][0] + gp * p0[1][0]
 
-    return q, p
+    H = np.zeros([len(t)])
+    for i in range(len(t)):
+        H[i] = ((p[0][i]) ** 2 + (p[1][i]) ** 2) / 2 - mu / (((q[0][i]) ** 2 + (q[1][i]) ** 2) ** (1 / 2))
+
+    return q, p, H
