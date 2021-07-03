0ad829583fbfaf9bb9e0e7923bc7c9ca8d5ac47b
AdityaWadichar <adityawadichar85@gmail.com>
2021-07-03 23:49:49 +0530

    Langrangian dynamics added

--- Five_link/five_link.py
+++ Five_link/five_link.py
@@ -77,6 +77,139 @@ def position(q, O):
 
 
 
+def dynamics(q, dq):
+    q1, q2, q3, q4, q5 = q[:]
+    dq1, dq2, dq3, dq4, dq5 = dq[:]
+
+
+    # first link
+
+    a11 = ((m1/3) + m2 + m3 + m4 + m5)*(l1**2)
+    a12 = ((m2/2) + m3 + m4 + m5)*l1*l2*np.cos(q1-q2)
+    a13 = (1/2)*m3*l1*l3*np.cos(q1-q3)
+    a14 = -((m4/2) + m5)*l1*l4*np.cos(q1-q4)
+    a15 = -(1/2)*m5*l1*l5*np.cos(q1-q5)
+
+    b11 = 0
+    b12 = -((m2/2) + m3 + m4 + m5)*l1*l2*(np.sin(q1-q2))*(dq1-dq2)*dq2
+    b13 = -(m3/2)*l1*l3*(np.sin(q1-q3))*(dq1-dq3)*dq3
+    b14 = ((m4/2) + m5)*l1*l4*(np.sin(q1-q4))*(dq1-dq4)*dq4
+    b15 = (m5/2)*l1*l5*(np.sin(q1-q5))*(dq1-dq5)*dq5
+    b1 = b11 + b12 + b13 + b14 + b15
+
+    c11 = ((m1/2) + m2 + m3 + m4 + m5)*g*l1*np.sin(q1)
+    c12 = -((m2/2) + m3 + m4 + m5)*l1*l2*dq1*dq2*np.sin(q1-q2)
+    c13 = -(m3/2)*l1*l3*dq1*dq3*np.sin(q1-q3)
+    c14 = ((m4/2) + m5)*l1*l4*dq1*dq4*np.sin(q1-q4)
+    c15 = (m5/2)*l1*l5*dq1*dq5*np.sin(q1-q5)
+    c1 = c11 + c12 + c13 + c14 + c15
+
+    # second link
+
+    a21 = ((m2/2) + m3 + m4 + m5)*l1*l2*np.cos(q1-q2)
+    a22 = ((m2/3) + m3 + m4 + m5)*(l2**2)
+    a23 = (m3/2)*l2*l3*mp.cos(q2-q3)
+    a24 = -((m4/2) + m5)*l2*l4*np.cos(q2-q4)
+    a25 = -(m5/2)*l2*l5*np.cos(q2-q5)
+
+    b21 = -((m2/2) + m3 + m4 + m5)*l1*l2*(np.sin(q1-q2))*(dq1-dq2)*dq1
+    b22 = 0
+    b23 = -(m3/2)*l2*l3*(np.sin(q2-q3))*(dq2-dq3)*dq3
+    b24 = ((m4/2) + m5)*l2*l4*(np.sin(q2-q4))*(dq2-dq4)*dq4
+    b25 = (m5/2)*l2*l5*(np.sin(q2-q5))*(dq2-dq5)*dq5
+    b2 = b21 + b22 + b23 + b24 + b25
+
+    c21 = ((m2/2) + m3 + m4 + m5)*l1*l2*dq1*dq2*np.sin(q1-q2)
+    c22 = ((m2/2) + m3 + m4 + m5)*g*l2*np.sin(q2)
+    c23 = -(m3/2)*l2*l3*dq2*dq3*np.sin(q2-q3)
+    c24 = ((m4/2) + m5)*l2*l4*dq2*dq4*np.sin(q2-q4)
+    c25 = (m5/2)*l2*l5*dq2*dq5*np.sin(q2-q5)
+    c2 = c21 + c22 + c23 + c24 + c25
+
+
+    # third link
+
+    a31 = (m3/2)*l1*l3*np.cos(q1-q3)
+    a32 = (m3/2)*l2*l3*np.cos(q2-q3)
+    a33 = (m3/3)*(l3**2)
+    a34 = 0
+    a35 = 0
+
+    b31 = (m3/2)*l1*l3*(np.sin(q1-q3))*(dq1-dq3)*dq1
+    b32 = (m3/2)*l2*l3*(np.sin(q2-q3))*(dq2-dq3)*dq2
+    b33=0
+    b34=0
+    b35=0
+    b3 = b31 + b32 + b33 + b34 + b35
+
+    c33 = (m3/2)*g*l3*np.sin(q3)
+    c32 = (m3/2)*l2*l3*dq2*dq3*np.sin(q2-q3)
+    c31 = (m3/2)*l1*l3*dq1*dq3*np.sin(q1-q3)
+    c34=0
+    c35=0
+    c3 = c31 + c32 + c33 + c34 + c35
+
+    # fourth link
+
+    a41 = -((m4/2) + m5)*l1*l4*np.cos(q1-q4)
+    a42 = -((m4/2) + m5)*l2*l4*np.cos(q2-q4)
+    a43 = 0
+    a44 = ((m4/3) + m5)*(l4**2)
+    a45 = (m5/2)*l4*l5*np.cos(q4-q5)
+
+    b41 = ((m4/2) + m5)*l1*l4*(np.sin(q1-q4))*(dq1-dq4)*dq1
+    b42 = ((m4/2) + m5)*l2*l4*(np.sin(q2-q4))*(dq2-dq4)*dq2
+    b43 = 0
+    b44 = 0
+    b45 = -(m5/2)*l4*l5*(np.sin(q4-q5))*(dq4-dq5)*dq5
+    b4 = b41 + b42 + b43 + b44 + b45
+
+    c44 = -((m4/2) + m5)*g*l4*np.sin(q4)
+    c41 = -((m4/2) + m5)*l1*l4*dq1*dq4*np.sin(q1-q4)
+    c42 = -((m4/2) + m5)*l2*l4*dq1*dq4*np.sin(q2-q4)
+    c43 = 0
+    c45 = -(m5/2)*l4*l5*dq4*dq5*np.sin(q4-q5)
+    c4 = c41 + c42 + c43 + c44 + c45
+
+
+    # fifth link
+
+    a51 = -(m5/2)*l1*l5*np.cos(q1-q5)
+    a52 = -(m5/2)*l2*l5*np.cos(q2-q5)
+    a53 = 0
+    a54 = (m5/2)*l4*l5*np.cos(q4-q5)
+    a55 = (m5/3)*(l5**2)
+
+    b51 = (m5/2)*l1*l5*(np.sin(q1-q5))*(dq1-dq5)*dq1
+    b52 = (m5/2)*l2*l5*(np.sin(q2-q5))*(dq2-dq5)*dq2
+    b53 = 0
+    b54 = -(m5/2)*l4*l5*(np.sin(q4-q5))*(dq4-dq5)*dq4
+    b55 = 0
+    b5 = b51 + b52 + b53 + b54 + b55
+
+    c51 = -(m5/2)*l1*l5*dq1*dq5*np.sin(q1-q5)
+    c52 = -(m5/2)*l2*l5*dq2*dq5*np.sin(q2-q5)
+    c53 = 0
+    c54 = (m5/2)*l4*l5*dq4*dq5*np.sin(q4-q5)
+    c55 = 0
+    c5 = c51 + c52 + c53 + c54 + c55
+
+
+    # matrix equations
+
+    A = np.matrix(((a11, a12, a13, a14, a15), (a21, a22, a23, a24, a25), (a31, a32, a33, a34, a35), (a41, a42, a43, a44, a45), (a51, a52, a53, a54, a55)))
+    B = np.matrix((b1, b2, b3, b4, b5))
+    B = B.transpose()
+    C = np.matrix((c1, c2, c3, c4, c5))
+    C = C.transpose()
+    T = np.matrix((0, 0, 0, 0, 0))
+    T = T.transpose()
+
+    X = (A**-1)*(T+C-B)
+
+    return X
+
+
 
 def stormer_verlet(q0, p0, N, h, O):
     q = np.zeros([5, N])
@@ -184,9 +317,11 @@ def animation(linkx, linky):
 
 q, p, linkx, linky, KE, PE, H = stormer_verlet(q0, p0, N, h, O)
 t=np.arange(len(H))
-plt.plot(h*t, H, label="H")
-plt.plot(h*t, KE, label="KE")
-plt.plot(h*t, PE, label="PE")
+plt.plot(h*t, H, label="Total Energy")
+plt.plot(h*t, KE, label="Kinetic Energy")
+plt.plot(h*t, PE, label="Potential Energy")
+plt.xlabel("t")
+plt.ylabel("Energy")
 plt.legend()
 plt.show()
 #animation(linkx, linky)
